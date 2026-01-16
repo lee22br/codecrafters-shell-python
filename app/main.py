5972206989ec0261a6964fdc66611050cf25dc2c
leandro.iglezias <leeiglezias@gmail.com>
2026-01-15 22:33:47 -0300

    Fase 3

--- app/main.py
+++ app/main.py
@@ -2,9 +2,18 @@ import sys
 
 
 def main():
-     sys.stdout.write("$ ")
-     command = input()
-     print(f"{command}: command not found")
+    command = ""
+    while (True):
+        sys.stdout.write("$ ")
+        command = input()
+        if (command == "exit"):
+            return
+        if (command.startswith("echo ")):
+            print(command.removeprefix("echo "))
+        else:
+            print(f"{command}: command not found")
+
+
 
 if __name__ == "__main__":
     main()
