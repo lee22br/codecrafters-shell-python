bc5ceaa8f27d2a2b5160376ed29bce848330f75f
leandro.iglezias <leeiglezias@gmail.com>
2026-01-16 21:49:14 -0300

    Fase 4

--- app/main.py
+++ app/main.py
@@ -3,16 +3,27 @@ import sys
 
 def main():
     command = ""
+
     while (True):
         sys.stdout.write("$ ")
         command = input()
-        if (command == "exit"):
-            return
-        if (command.startswith("echo ")):
-            print(command.removeprefix("echo "))
-        else:
-            print(f"{command}: command not found")
+        commands = command.split()
+        match commands[0]:
+            case "exit":
+                exit(0)
+            case "echo":
+                print(command.removeprefix("echo "))
+            case "type":
+                type(commands[1])
+            case _:
+                print(f"{command}: command not found")
 
+def type(command):
+    builtins = ["exit","echo","type"]
+    if command in builtins:
+        print(f"{command} is a shell builtin")
+    else:
+        print(f"{command}: not found")
 
 
 if __name__ == "__main__":
